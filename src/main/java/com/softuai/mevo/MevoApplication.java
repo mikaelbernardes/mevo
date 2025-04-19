dce4c01b9667dc281628aadd90cff8f557893a6d
Mikael Bernardes <mikaelbernardes.dev@gmail.com>
2025-04-18 22:33:02 -0300

    first commit

---
+++ src/main/java/com/softuai/mevo/MevoApplication.java
@@ -0,0 +1,13 @@
+package com.softuai.mevo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MevoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MevoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softuai/mevo/MevoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuai.mevo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MevoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
